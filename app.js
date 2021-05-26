4ffa56120b927e3bfdc13c6c670f233db696f9b1
Javan <javan510.gmail.com>
2021-05-25 21:41:17 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var cities = document.querySelectorAll(".city");
+var btn = document.querySelector(".cookieBox");
+
+var right = document.querySelectorAll(".rightTemp");
+var left = document.querySelectorAll(".leftTemp");
+
+
+function alertCity () {
+  alert('Loading Weather report...');
+}
+
+function accepted () {
+  btn.remove();
+}
